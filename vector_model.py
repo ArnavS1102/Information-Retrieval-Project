cb7aa0e778f1a6201c3111507c1ab7f5e108f634
ArnavS1102 <arnsharma02@gmail.com>
2025-04-15 00:27:34 -0500

    Updated Vector Model

--- vector_model.py
+++ vector_model.py
@@ -77,7 +77,7 @@ class BertKNNVectorModel:
         distances, indices = self.knn_index.kneighbors(query_embedding)
 
         results = self.indexed_df.iloc[indices[0]].copy()
-        # results['score'] = 1 - distances[0]  # Cosine similarity
+        results['score'] = 1 - distances[0]  # Cosine similarity
 
         def resolve_description(row):
             if pd.isna(row['meta_description']) or not row['meta_description'] or row['meta_description']=='No Description':
@@ -88,17 +88,17 @@ class BertKNNVectorModel:
         results['meta_description'] = results.apply(resolve_description, axis=1)
 
         # Convert to JSON-serializable output
-        # json_results = results[['url', 'title', 'meta_description', 'score']].to_dict(orient='records')
-        json_results = results[['url', 'title', 'meta_description']].to_dict(orient='records')
+        json_results = results[['url', 'title', 'meta_description', 'score']].to_dict(orient='records')
+        # json_results = results[['url', 'title', 'meta_description']].to_dict(orient='records')
         return json_results
 
 
-if __name__ == "__main__":
-    model = BertKNNVectorModel("combined_data.csv", cache_dir="cache")
-    # model.preprocess_and_index()
-    query = input("Enter your search query: ")
-    results = model.search(query)
-    print(results)
+# if __name__ == "__main__":
+#     model = BertKNNVectorModel("combined_data.csv", cache_dir="cache")
+#     # model.preprocess_and_index()
+#     query = input("Enter your search query: ")
+#     results = model.search(query)
+#     print(results)
 
     # print("\nTop Results:\n")
     # for i, (url, score) in enumerate(zip(results['url'], scores)):
