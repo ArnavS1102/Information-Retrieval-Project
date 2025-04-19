0bf30499e8822f1a46ec88ed2c52d296bed4e954
ArnavS1102 <arnsharma02@gmail.com>
2025-04-19 00:49:08 -0500

    Added Cleaning

--- clean_data.py
+++ clean_data.py
@@ -3,10 +3,32 @@ import pandas as pd
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 from vector_model import BertKNNVectorModel
-
 from link_model import SearchEngine
 
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Download NLTK resources
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
 load_dotenv()
+
+# Text cleaning setup
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+    if pd.isna(text):
+        return ""
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)  
+    words = text.split()
+    words = [lemmatizer.lemmatize(w) for w in words if w not in stop_words]
+    return ' '.join(words)
 
 def get_directory_and_slug(url):
     path = urlparse(url).path.rstrip('/')
@@ -40,6 +62,9 @@ if __name__ == "__main__":
     df = df.sort_values("last_crawled").drop_duplicates(subset="url", keep="first")
     df.dropna(subset=['url', 'body_text'], inplace=True)
 
+    # Clean the body_text
+    df['body_text'] = df['body_text'].apply(clean_text)
+
     df['url_key'] = df['url'].apply(get_url_key)
     df = df.drop_duplicates(subset='url_key').drop(columns='url_key')
 
@@ -65,10 +90,3 @@ if __name__ == "__main__":
 
     #For Hybrid
     # results = link_engine.hybrid_model(query)
-
-
-
-
-
-
-
